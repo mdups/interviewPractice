f1642e4fe88bd2ff7f84993f6dc3e910254be4f6
mdups <dupee.m@northeastern.edu>
2022-11-01 13:31:59 -0400

    color guessing challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [Color, SetColor] = useState('#aaaaaa');
+  const [Colors, SetColors] = useState([]);
+  const [LastGuess, SetLastGuess] = useState('');
+  const [ShowBubble, SetShowBubble] = useState(0);
+
+  const colorGenerator = () => {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return `#${randomColor}`;
+  }
+
+  const colorsList = () =>{
+    let colorArr = [colorGenerator(), colorGenerator(), colorGenerator()];
+    colorArr[Math.floor(Math.random() * 3)] = Color;
+    return colorArr;
+  }
+
+  // Empty dependency array to only do at the beginning of the code being run
+  useEffect(()=> {
+    SetColor(colorGenerator());
+    }, []);
+
+  // This uses Color in a dependency array because it will update whenever color updates,
+  // if it ran in the same useEffect as SetColor, at times SetColor would not update fast enough
+  // and this would lead a bug with the original Color state being used
+  useEffect(()=>{
+    SetColors(colorsList());
+    }, [Color]);
+
+  useEffect(()=>{
+    if(LastGuess == Color){
+      SetColor(colorGenerator());
+      SetShowBubble(1);
+    }
+    else if (LastGuess !== ''){
+      SetShowBubble(2);
+    }
+  }, [LastGuess])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Color' style = {{background: Color}}></div>
+      <div id="buttons">
+       {
+        Colors.map((col) => {
+          return <button onClick={ () =>{
+              SetLastGuess(col);
+            }
+          }
+          > {col} </button>
+        })
+       }
+      </div>
+      <div>
+        {
+          ShowBubble == 1 ? <div id='correct'> Correct </div>: null
+        }
+        {
+          ShowBubble === 2 ? <div id='wrong'> Wrong! </div>: null
+        }
+      </div>
     </div>
   );
 }
